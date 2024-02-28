ecf390348dffb83f52fcb02eefc237795d7590f2
Leopold95 <leopold9586@gmail.com>
2024-02-28 14:18:17 +0200

    updates

--- src/main/java/me/leopold95/funutils/core/oregeneration/OreVien.java
+++
@@ -1,9 +0,0 @@
-package me.leopold95.funutils.core.oregeneration;
-
-public class OreVien {
-	public int chance;
-	public int amount_from;
-	public int amount_to;
-	public  int height_from;
-	public int height_to;
-}
