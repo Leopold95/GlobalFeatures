3001f793f45341d770139c1b23506c39ed5f601e
Leopold95 <leopold9586@gmail.com>
2024-03-07 08:47:29 +0300

    updates

--- src/main/java/me/leopold95/funutils/FunUtils.java
+++ src/main/java/me/leopold95/funutils/FunUtils.java
@@ -7,6 +7,7 @@ import me.leopold95.funutils.listeners.BlockPlaceListener;
 import me.leopold95.funutils.listeners.ChunkGeneratedListener;
 import me.leopold95.funutils.listeners.EntityDeathListener;
 import me.leopold95.funutils.utils.Config;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FunUtils extends JavaPlugin {
@@ -17,7 +18,7 @@ public final class FunUtils extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-
+        
         getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
         getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
